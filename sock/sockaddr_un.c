6955f1ae23659ad536a027721af2722a0a56cae2
qcx761 <3216563979@qq.com>
2025-04-17 22:49:01 +0800

    1

--- sock/sockaddr_un.c
+++ sock/sockaddr_un.c
@@ -8,6 +8,11 @@
 int main() {
     int sockfd;
     struct sockaddr_un server_addr;
+
+    //struct sockaddr_un {
+    //     sa_family_t sun_family;         // 地址族，定义了套接字将使用的协议类型和地址格式，通常为 AF_UNIX
+    //     char        sun_path[108];      // 套接字路径
+    // };
     
     // 创建 Unix Domain 套接字
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
