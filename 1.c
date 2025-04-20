325f347395bc79c259531b26e09b8d17ce686595
qcx761 <3216563979@qq.com>
2025-04-20 20:36:46 +0800

    1

--- 1.c
+++ 1.c
@@ -1,4 +1,24 @@
-11111111111111111
-22222222222
-3333333
-444444
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main() {
+    const char *hostname = "www.example.com";
+    struct hostent *he;
+    struct in_addr addr;
+
+    // 获取主机信息
+    he = gethostbyname(hostname);
+    if (he == NULL) {
+        herror("gethostbyname");
+        return 1;
+    }
+
+    // 转换地址
+    memcpy(&addr, he->h_addr_list[0], sizeof(struct in_addr));
+    printf("IP address: %s\n", inet_ntoa(addr));
+
+    return 0;
+}
