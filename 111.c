09f92931dcad4583fc0a9740f8ecc668532a941e
qcx761 <3216563979@qq.com>
2025-05-07 19:51:48 +0800

    1

--- 111.c
+++ 111.c
@@ -11,3 +11,20 @@ int main(){
     return 0;
 }
 
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    *returnSize = temperaturesSize;
+    int* ans = calloc(temperaturesSize, sizeof(int));
+    int* st = malloc(temperaturesSize * sizeof(int)); // 用数组模拟栈
+    int top = -1; // 栈顶下标
+    for (int i = 0; i < temperaturesSize; i++) {
+        int t = temperatures[i];
+        while (top >= 0 && t > temperatures[st[top]]) {
+            int j = st[top--]; // 弹出栈顶
+            ans[j] = i - j;
+        }
+        st[++top] = i; // 入栈
+    }
+    free(st);
+    return ans;
+}
+
