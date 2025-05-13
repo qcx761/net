a73cb7bbc3c221928b42eb7a79ace167d928f999
qcx761 <3216563979@qq.com>
2025-05-13 19:24:30 +0800

    1

--- FTP/server.c
+++ FTP/server.c
@@ -10,6 +10,37 @@
 #include <fcntl.h>
 
 #define PORT 2100
+#define PORT1 5000
+
+void handle_client(int control_socket) {
+    char buffer[1024];
+    
+    while(1){
+        // 清空缓冲区
+        memset(buffer,0,sizeof(buffer));
+        // 接收客户端消息
+        int received=recv(control_socket,buffer,sizeof(buffer)-1,0);
+        if(received<=0){
+            break;
+        }
+        
+        // 处理 PASV 命令
+        if (strncmp(buffer, "PASV", 4) == 0) {
+            // 生成数据端口并回复客户端
+            int h1 = 192, h2 = 168, h3 = 1, h4 = 1;
+            int p1 = PORT1 / 256;
+            int p2 = PORT1 % 256;
+            sprintf(buffer, "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)\r\n", h1, h2, h3, h4, p1, p2);
+            send(control_socket, buffer, strlen(buffer), 0);
+        }
+        
+        // 处理其他命令...
+        // 例如 USER、PASS、LIST 等
+    }
+    
+    // 关闭控制连接
+    close(control_socket);
+}
 
 int main(){
     int server_fd;
@@ -40,5 +71,12 @@ int main(){
         exit(-1);
     }
 
+    while (1){
+        int control_socket = accept(server_fd, NULL, NULL);
+        handle_client(control_socket);
+    }
+
+    close(server_fd);
+    return 0;
 
 }
